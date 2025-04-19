d3a3b219a4fb8963672ca0da7235192aa8be9418
Danil Sidoruk <danilsidoruknv@yandex.ru>
2025-04-19 17:40:04 +0300

    Fix deep_ptr interface

--- include/deep_ptr/deep_ptr.hpp
+++ include/deep_ptr/deep_ptr.hpp
@@ -3,20 +3,21 @@
 template <typename... Ts> struct deep_ptr {
   constexpr deep_ptr(Ts... args) : ptrs{args...} {}
 
+  template <typename U> constexpr auto get(U &&object) {
+    return get_impl(object, std::make_index_sequence<sizeof...(Ts)>{});
+  }
+
+private:
   template <typename U, std::size_t I>
-  auto get_impl(U &&object, std::index_sequence<I>) {
+  constexpr auto get_impl(U &&object, std::index_sequence<I>) {
     return (std::forward<U>(object)).*(std::get<I>(ptrs));
   }
 
   template <typename U, std::size_t I1, std::size_t... IRest>
-  auto get_impl(U &&object, std::index_sequence<I1, IRest...>) {
+  constexpr auto get_impl(U &&object, std::index_sequence<I1, IRest...>) {
     return get_impl(
         get_impl<U>(std::forward<U>(object), std::index_sequence<I1>{}),
         std::index_sequence<IRest...>{});
-  }
-
-  template <typename U> auto get(U &object) {
-    return get_impl(object, std::make_index_sequence<sizeof...(Ts)>{});
   }
 
   std::tuple<Ts...> ptrs;
