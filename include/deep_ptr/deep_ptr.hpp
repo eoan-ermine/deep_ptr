48c82afc697a92970f9566eb0e3c6f826db76d71
Danil Sidoruk <danilsidoruknv@yandex.ru>
2026-03-20 13:42:14 +0300

    Fix return value of deep_ptr::get

--- include/deep_ptr/deep_ptr.hpp
+++ include/deep_ptr/deep_ptr.hpp
@@ -1,22 +1,27 @@
 #include <tuple>
 
 template <typename... Ts> struct deep_ptr {
+  static_assert(sizeof...(Ts) > 0,
+                "deep_ptr requires at least one template parameter");
+
   constexpr deep_ptr(Ts... args) : ptrs{args...} {}
 
-  template <typename U> constexpr auto get(U &&object) {
-    return get_impl(object, std::make_index_sequence<sizeof...(Ts)>{});
+  template <typename U> constexpr decltype(auto) get(U &&object) const {
+    return get_impl(std::forward<U>(object),
+                    std::make_index_sequence<sizeof...(Ts)>{});
   }
 
 private:
   template <typename U, std::size_t I>
-  constexpr auto get_impl(U &&object, std::index_sequence<I>) {
-    return (std::forward<U>(object)).*(std::get<I>(ptrs));
+  constexpr decltype(auto) get_impl(U &&object, std::index_sequence<I>) const {
+    return std::forward<U>(object).*std::get<I>(ptrs);
   }
 
   template <typename U, std::size_t I1, std::size_t... IRest>
-  constexpr auto get_impl(U &&object, std::index_sequence<I1, IRest...>) {
+  constexpr decltype(auto) get_impl(U &&object,
+                                    std::index_sequence<I1, IRest...>) const {
     return get_impl(
-        get_impl<U>(std::forward<U>(object), std::index_sequence<I1>{}),
+        get_impl(std::forward<U>(object), std::index_sequence<I1>{}),
         std::index_sequence<IRest...>{});
   }
 
